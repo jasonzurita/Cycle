25d1b9c865ff1bde2ee05ebf4cb5e872f761a096
Jason Zurita <jasonzurita@me.com>
2020-12-19 21:41:05 -0500

    feat: add test facilitating function
    - Consider moving this out to a separate module
    so that all importers of `Cycle` don't have access to this.

---
+++ Sources/Cycle/TestSupport.swift
@@ -0,0 +1,96 @@
+#if !os(watchOS)
+import Combine
+import XCTest
+
+public enum StepType<Action> {
+    case send(Action)
+    case receive(Action)
+    case receiveFireAndForget
+}
+
+public struct Step<Value, Action> {
+    let file: StaticString
+    let line: UInt
+    let type: StepType<Action>
+    let update: (inout Value) -> Void
+
+    public init(
+        file: StaticString = #filePath,
+        line: UInt = #line,
+        _ type: StepType<Action>,
+        delta: @escaping (inout Value) -> Void
+    ) {
+        self.file = file
+        self.line = line
+        self.type = type
+        self.update = delta
+    }
+}
+
+public func assert<Value: Equatable, Action: Equatable>(
+    initialValue: Value,
+    reducer: (inout Value, Action) -> [Effect<Action>],
+    steps: Step<Value, Action>...,
+    file: StaticString = #filePath,
+    line: UInt = #line
+) {
+    var value = initialValue
+    var effects: [Effect<Action>] = []
+    var cancellables: [AnyCancellable] = []
+
+    steps.forEach { step in
+        var expected = value
+
+        switch step.type {
+        case let .send(stepAction):
+            guard effects.isEmpty else {
+                XCTFail("Action sent before handling \(effects.count) pending effect(s).", file: file, line: line)
+                return
+            }
+            effects.append(contentsOf: reducer(&value, stepAction))
+        case let .receive(stepAction):
+            guard !effects.isEmpty else {
+                XCTFail("No pending effects to receive", file: file, line: line)
+                return
+            }
+            let effect = effects.removeFirst()
+            var action: Action!
+            let receivedCompletion = XCTestExpectation(description: "receivedCompletion")
+
+            effect.sink { _ in
+                receivedCompletion.fulfill()
+            } receiveValue: { action = $0 }
+            .store(in: &cancellables)
+
+            if XCTWaiter.wait(for: [receivedCompletion], timeout: 0.01) != .completed {
+                XCTFail("Timed out waiting for the effect to complete.", file: step.file, line: step.line)
+            }
+            XCTAssertEqual(action, stepAction, file: step.file, line: step.line)
+            if let a = action {
+                effects.append(contentsOf: reducer(&value, a))
+            }
+        case .receiveFireAndForget:
+            guard !effects.isEmpty else {
+                XCTFail("No pending effects to receive", file: file, line: line)
+                return
+            }
+            let effect = effects.removeFirst()
+            let receivedCompletion = XCTestExpectation(description: "receivedCompletion")
+            effect.sink { _ in
+                receivedCompletion.fulfill()
+            } receiveValue: { _ in
+                XCTFail("No value should have been received from a fire and forget effect", file: file, line: line)
+            }
+            .store(in: &cancellables)
+
+            if XCTWaiter.wait(for: [receivedCompletion], timeout: 0.01) != .completed {
+                XCTFail("Timed out waiting for the effect to complete.", file: step.file, line: step.line)
+            }
+        }
+        step.update(&expected)
+        XCTAssertEqual(value, expected, file: step.file, line: step.line)
+    }
+
+    XCTAssertTrue(effects.isEmpty, "Assertion failed to handle \(effects.count) pending effect(s).", file: file, line: line)
+}
+#endif
